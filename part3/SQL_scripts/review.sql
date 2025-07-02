11d42b15d255e439931cadab7624a4eb05c87e58
Fahad <10679@holbertonstudents.com>
2025-07-02 22:57:20 +0300

    Update review.sql

--- part3/SQL_scripts/review.sql
+++ part3/SQL_scripts/review.sql
@@ -1,7 +1,7 @@
 CREATE TABLE review (
     id UUID PRIMARY KEY,
     _text TEXT,
-    _rating INT CHECK (rating BETWEEN 1 AND 5),
+    _rating INT CHECK (_rating BETWEEN 1 AND 5),
     _user_id CHAR(36) UNIQUE,
     _place_id CHAR(36) UNIQUE,
     FOREIGN KEY (_user_id) REFERENCES user(id),
