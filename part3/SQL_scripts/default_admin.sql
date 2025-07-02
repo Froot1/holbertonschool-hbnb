49d9ec2e295e1b6912836b7899827984366a3a8d
root <10679@holbertonstudents.com>
2025-07-02 13:30:17 +0000

    update

--- part3/SQL_scripts/default_admin.sql
+++ part3/SQL_scripts/default_admin.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (id, _email, _first_name, _last_name, _password, _is_admin) VALUES (
     'b2fb6ab3-1ec8-444b-8a91-a70bebf8eb52',
-    'Fahad@test.com',
+    'fahad@test.com',
     'Fahad',
     'Admin',
     '$2a$13$iWtaBthhmLqjmChZVjN7EufspWbwwbNtz7wjWdx0om8MiW9UzZ54q',
