9cdae7d1bd012fc47dd950319162e3f0ad31ce7c
Fahad <10679@holbertonstudents.com>
2025-07-02 22:32:53 +0300

    Update user.sql

--- part3/SQL_scripts/user.sql
+++ part3/SQL_scripts/user.sql
@@ -5,5 +5,7 @@ CREATE TABLE user (
     _email VARCHAR(255) UNIQUE,
     _password VARCHAR(255),
     _is_admin BOOLEAN DEFAULT FALSE
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
