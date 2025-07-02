931d40314f124166dd96f73cf11d4adf06c26cc0
Fahad <10679@holbertonstudents.com>
2025-07-02 22:04:47 +0300

    Update default_admin.sql

--- part3/SQL_scripts/default_admin.sql
+++ part3/SQL_scripts/default_admin.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (id, _email, _first_name, _last_name, _password, _is_admin) VALUES (
+INSERT INTO user (id, _email, _first_name, _last_name, _password, _is_admin) VALUES (
     '36c9050e-ddd3-4c3b-9731-9f487208bbc1',
     'Fahad@test.com',
     'Admin',
