47e613c8e2adaa745bcb04149c793a8145d1d1cd
Fahad <10679@holbertonstudents.com>
2025-07-02 21:33:55 +0300

    Update default_admin.sql

--- part3/SQL_scripts/default_admin.sql
+++ part3/SQL_scripts/default_admin.sql
@@ -1,8 +1,8 @@
 INSERT INTO users (id, _email, _first_name, _last_name, _password, _is_admin) VALUES (
-    'b2fb6ab3-1ec8-444b-8a91-a70bebf8eb52',
-    'fahad@test.com',
-    'Fahad',
+    '36c9050e-ddd3-4c3b-9731-9f487208bbc1',
+    'Fahad@test.com',
     'Admin',
+    'Fahad',
     '$2a$13$iWtaBthhmLqjmChZVjN7EufspWbwwbNtz7wjWdx0om8MiW9UzZ54q',
     TRUE
 );
