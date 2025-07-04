a12db89401077dc63a221eab6aae0e41777a40d3
root <10679@holbertonstudents.com>
2025-07-04 23:08:20 +0000

    update

--- part3/SQL_scripts/default_admin.sql
+++ part3/SQL_scripts/default_admin.sql
@@ -1,8 +1,8 @@
 INSERT INTO users (id, _email, _first_name, _last_name, _password, _is_admin) VALUES (
     '36c9050e-ddd3-4c3b-9731-9f487208bbc1',
-    'Fahad@test.com',
+    'admin@hbnb.io',
     'Admin',
-    'Fahad',
+    'HBnb',
     '$2a$13$iWtaBthhmLqjmChZVjN7EufspWbwwbNtz7wjWdx0om8MiW9UzZ54q',
     TRUE
 );
